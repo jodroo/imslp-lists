5fe92445eeae8222c356d405cd0dcd8c198e4f2f
Wolfgang Esser-Skala <wolfgang@esser-skala.at>
2024-10-05 10:50:27 +0200

    updates list creator to allow for different lists

--- make_list.py
+++ make_list.py
@@ -1,35 +1,14 @@
 """Convert CSV to IMSLP Composer Composition List"""
 
 import re
+import sys
 from typing import Union
 import pandas as pd
+import strictyaml  # type: ignore
 
-
-PAGE_HEADER = """\
-{{worklist|Haydn, Michael}}
-
-{{Bluebox|'''Note''': This page is semi-automatically generated from [https://github.com/edition-esser-skala/imslp-lists/blob/main/data/michael_haydn.csv this table]. Thus, any changes or corrections should be preferably entered in the source table.}}
-
-The table below gives the following information (where applicable):
-* '''MH''' — numbering as given in Charles H. Sherman and T. Donley Thomas, Johann Michael Haydn (1737–1806). A Chronological Thematic Catalogue of His Works (Stuyvesant, New York, 1993).
-* '''Title''' — as used on IMSLP.
-* '''Key''' — the principal key of the work.
-* '''Date''' — the year(s) of composition or arrangement, where known.
-* '''Genre''' — as used by IMSLP's categorization system.
-* '''Scoring''' — the instrumentation used.
-* '''Notes''' — concerning related works, completeness, authorship, etc.
-
-{| class="sortable wikitable" style="font-size:95%; width: 98%; vertical-align: top; border: 1px solid #AAA"
-! width="6%"|MH
-! width="35%"|Title
-! width="6%"|Key
-! width="10%"|Date
-! width="6%"|Genre
-! width="37%"|Notes
-|-"""
 
 TABLE_ROW_TEMPLATE = """
-| {mh}
+| {id}
 | {title}
 | {key}
 | {date}
@@ -43,12 +22,12 @@ PAGE_FOOTER = """\
 
 
 def format_catalogue_number(n: Union[int, str]) -> str:
-    """Formats the MH number with leading zeros."""
+    """Formats the catalogue number with leading zeros."""
     lit = ""
     if isinstance(n, str):
         m = re.match(r"([0-9]+)(.?)", n)
         if m is None:
-            raise ValueError("Wrong MH format.")
+            raise ValueError("Wrong catalogue number format.")
         n = int(m[1])
         lit = m[2]
     if n < 10:
@@ -58,11 +37,16 @@ def format_catalogue_number(n: Union[int, str]) -> str:
     return f"{n}{lit}"
 
 
-def format_title(title_imslp: str, title: str) -> str:
+def format_title(title_imslp: str,
+                 title: str,
+                 composer_imslp: str,
+                 default_composer_imslp: str) -> str:
     """Adds a link to the work page (if available)."""
+    if pd.isna(composer_imslp):
+        composer_imslp = default_composer_imslp
     if pd.isna(title_imslp):
         return title
-    return f"{{{{LinkWorkN|{title_imslp}||Haydn|Michael|0}}}}"
+    return f"{{{{LinkWorkN|{title_imslp}||{composer_imslp}|0}}}}"
 
 
 def format_key(key: str) -> str:
@@ -79,15 +63,27 @@ def format_date(date: Union[str, pd.Timestamp]) -> str:
 
 def main() -> None:
     """Main function."""
+    list_name = sys.argv[1]
 
-    works = pd.read_csv("data/michael_haydn.csv")
+    try:
+        works = pd.read_csv(f"data/{list_name}.csv")
+        with open(f"config/{list_name}.yaml", encoding="utf8") as f:
+            config = strictyaml.load(f.read()).data
+    except IndexError:
+        print("Error: No list specified.")
+        sys.exit(1)
+    except FileNotFoundError:
+        print(f"Error: Unknown list '{list_name}'.")
+        sys.exit(1)
 
     table_rows = [
         TABLE_ROW_TEMPLATE.format(
-            mh=format_catalogue_number(work.id),  # type: ignore[attr-defined]
+            id=format_catalogue_number(work.id),  # type: ignore[attr-defined]
             title=format_title(
                 work.title_imslp,  # type: ignore[attr-defined]
-                work.title  # type: ignore[attr-defined]
+                work.title,  # type: ignore[attr-defined]
+                work.composer_imslp,  # type: ignore[attr-defined]
+                config["composer_imslp"]
             ),
             key=format_key(work.key),  # type: ignore[attr-defined]
             date=format_date(work.date),  # type: ignore[attr-defined]
@@ -97,10 +93,11 @@ def main() -> None:
         for work in works.itertuples()
     ]
 
-    with open("output.txt", "w", encoding="utf8") as f:
-        f.write(PAGE_HEADER)
+    with open(f"output/{list_name}.txt", "w", encoding="utf8") as f:
+        f.write(config["page_header"].rstrip("\n"))
         f.write("|-".join(table_rows))
         f.write(PAGE_FOOTER)
+        print(f"Successfully wrote IMSLP list for '{list_name}'.")
 
 if __name__ == "__main__":
     main()
